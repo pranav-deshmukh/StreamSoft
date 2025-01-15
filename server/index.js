661e6a187e650b8c685829c6e72b953fb9f6d64a
Pranav Deshmukh <pranavdeshmukh190@gmail.com>
2025-01-15 10:09:50 +0530

    backend cjange

--- server/index.js
+++ server/index.js
@@ -4,17 +4,16 @@ import express from "express";
 import cors from "cors";
 import { spawn } from "child_process";
 import { Server as SocketIo } from "socket.io";
+import dotenv from "dotenv";
 
 const app = express();
 const server = http.createServer(app);
+dotenv.config();
 
 // Define allowed origins
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://streamsoft-streamsoft-deploy.up.railway.app",
-  "stream-soft-git-main-pranav-deshmukhs-projects.vercel.app",
-  // Add your production frontend URL if different
-];
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",")
+  : ["http://localhost:3000"];
 
 // Configure CORS with dynamic origin checking
 const corsOptions = {
